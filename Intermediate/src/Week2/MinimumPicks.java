01a98266aa1170f051493ff2aa9127c227c02c6e
Abhinav <ranaabhinav50@gmail.com>
2022-08-01 11:23:56 +0530

    Create MinimumPicks.java

---
+++ Intermediate/src/Week2/MinimumPicks.java
@@ -0,0 +1,35 @@
+package Week2;
+
+
+/*
+    Maximum Even - Minimum Even
+ */
+
+import java.util.ArrayList;
+
+public class MinimumPicks {
+
+    public int solve(ArrayList<Integer> A) {
+        int maxEven = Integer.MIN_VALUE;
+        int minOdd = Integer.MAX_VALUE;
+
+        for(int i = 0; i<A.size() ; i++) {
+            int num = A.get(i);
+            if ((num | 1) > num) {
+                if(num>maxEven){
+                    maxEven = num;
+                }
+//                System.out.println("Number is Even");
+            } else {
+                if(num<minOdd){
+                    minOdd = num;
+                }
+//                System.out.println("Number is Odd");
+            }
+        }
+        return maxEven- minOdd;
+
+
+    }
+
+}
