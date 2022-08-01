837ac23cb05c90017614a491420e63169b8801d0
Abhinav <ranaabhinav50@gmail.com>
2022-08-01 15:27:07 +0530

    Create SpecialSubsequences.java

---
+++ Intermediate/src/Week2/SpecialSubsequences.java
@@ -0,0 +1,20 @@
+package Week2;
+
+public class SpecialSubsequences {
+    public int solve(String A) {
+
+        long M = 1000000007;
+        long count = 0L;
+        long countA = 0L;
+        for(int i = 0 ; i<A.length(); i++){
+            if(A.charAt(i) == 'A'){
+                countA++;
+            }
+            else if(A.charAt(i) == 'G'){
+                count += countA;
+            }
+        }
+        return (int) (  count%M);
+
+    }
+}
