9a84b02dcd5ad5175b9cdf7560b35c112b185406
Abhinav <ranaabhinav50@gmail.com>
2022-08-02 16:11:45 +0530

    Create SwitchPress.java

---
+++ Intermediate/src/Week2/SwitchPress.java
@@ -0,0 +1,27 @@
+package Week2;
+
+import java.util.ArrayList;
+
+public class SwitchPress {
+
+    public int bulbs(ArrayList<Integer> A) {
+
+        int switchPress = 0;
+        int currState = 0;
+        for(int i = 0 ;i<A.size() ; i++){
+
+            if(switchPress%2==0){
+                currState = A.get(i);
+            }
+            else{
+                currState = A.get(i)==0?1:0;
+            }
+            if(currState == 0){
+                switchPress++;
+            }
+
+        }
+        return switchPress;
+    }
+
+}
