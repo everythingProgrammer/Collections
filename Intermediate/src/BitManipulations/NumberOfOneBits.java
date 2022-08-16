40c2edc762d5ea89966657682c0b96cd9b0b1326
Abhinav <ranaabhinav50@gmail.com>
2022-08-16 14:31:32 +0530

    Create NumberOfOneBits.java

---
+++ Intermediate/src/BitManipulations/NumberOfOneBits.java
@@ -0,0 +1,18 @@
+package BitManipulations;
+
+public class NumberOfOneBits {
+
+    public int numSetBits(int A) {
+
+        int count = 0;
+        while(A>0){
+             if((A&1) == 1){
+                 count++;
+             }
+            A = A>>1;
+        }
+        return count;
+    }
+
+
+}
