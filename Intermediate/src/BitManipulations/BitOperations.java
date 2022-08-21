4e7bd53058290f5e33b47a16f16eb3827e41c294
Abhinav <ranaabhinav50@gmail.com>
2022-08-21 17:52:02 +0530

    Create BitOperations.java

---
+++ Intermediate/src/BitManipulations/BitOperations.java
@@ -0,0 +1,18 @@
+package BitManipulations;
+
+public class BitOperations {
+
+    public static void Or(int a, int b){
+        System.out.println(a|b);
+
+    }
+    public static void And( int a , int b){
+        System.out.println(a&b);
+    }
+
+    public static void main(String[] args) {
+        Or(29,25);
+        And(29,25);
+    }
+
+}
