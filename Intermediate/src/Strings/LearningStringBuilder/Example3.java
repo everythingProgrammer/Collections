a216f91ad1fde299bc47643ec5e27d8e6511755c
Abhinav <ranaabhinav50@gmail.com>
2022-08-27 21:32:52 +0530

    Create Example3.java

---
+++ Intermediate/src/Strings/LearningStringBuilder/Example3.java
@@ -0,0 +1,16 @@
+package Strings.LearningStringBuilder;
+
+public class Example3 {
+
+
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("abcdefghijklmnopqrstuvwxyz");
+//        str = str.subSequence(0,1);
+//        str = str.subSequence()
+        System.out.println(str.substring(0));
+
+
+
+    }
+
+}
