d8034f1b002b2f86cdf55ac050162d6bbc5d0001
Abhinav <ranaabhinav50@gmail.com>
2022-09-10 20:18:16 +0530

    Create L75.java

---
+++ LeetCode/src/leetcode/L75.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+
+//https://leetcode.com/problems/sort-colors/
+public class L75 {
+
+    public void sortColors(int[] nums) {
+        // 1 solution being count number of 0's 1's and 2's
+
+        // 2nd solution using 2 pointer?
+
+
+
+
+    }
+}
