81310474923f7f17e132f568b75d9be5bbb06b71
Abhinav <ranaabhinav50@gmail.com>
2022-09-16 08:30:56 +0530

    Create TreeNode.java

---
+++ Intermediate/src/Trees/TreeNode.java
@@ -0,0 +1,13 @@
+package Trees;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){
+        val = 0;
+        left = null;
+        right  = null;
+    }
+}
