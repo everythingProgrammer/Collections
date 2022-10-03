a51d3cc43f1b92fe7daabdd4d4a48527ecda22f4
Abhinav <ranaabhinav50@gmail.com>
2022-10-03 14:39:20 +0530

    Create NumberOf1Bits.java

---
+++ Advance/src/Bitwise/NumberOf1Bits.java
@@ -0,0 +1,13 @@
+package Bitwise;
+
+public class NumberOf1Bits {
+    public int numSetBits(int A) {
+        int count = 0;
+        while( A>0 ){
+            if((A&1) == 1)
+                count++;
+            A = A>>1;
+        }
+        return count;
+    }
+}
