ab009c6d6e6441bacff466292b586cde8faa02c2
Abhinav <ranaabhinav50@gmail.com>
2026-04-25 19:37:49 +0530

    Update BasicRunnable.java

--- src/threading/Executors/exercise/BasicRunnable.java
+++ src/threading/Executors/exercise/BasicRunnable.java
@@ -1,4 +1,18 @@
 package threading.Executors.exercise;
 
+import java.util.concurrent.Executor;
+
 public class BasicRunnable {
+
+
+    public static void main(String args[]){
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Doing ");
+            }
+        };
+
+
+    }
 }
