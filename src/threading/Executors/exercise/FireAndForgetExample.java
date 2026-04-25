de714a137ad3bbaeb7d5d421d2660045953d713c
Abhinav <ranaabhinav50@gmail.com>
2026-04-25 22:06:01 +0530

    Update FireAndForgetExample.java

--- src/threading/Executors/exercise/FireAndForgetExample.java
+++ src/threading/Executors/exercise/FireAndForgetExample.java
@@ -1,18 +1,39 @@
 package threading.Executors.exercise;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class FireAndForgetExample {
 
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException, ExecutionException {
         ExecutorService service = Executors.newFixedThreadPool(2);
+        /*
+            Fire and forget Task
+         */
         Runnable task = ()->{
             System.out.println("Running in " + Thread.currentThread().getName());
         };
         service.execute(task);
+
+
+
+        /*
+            Fire and forget task using Future
+
+
+            Runnable has no return . 
+         */
+
+        Runnable task2 = ()->{
+            System.out.println("Task Running ");
+        };
+        Future<?> future = service.submit(task2);
+//        if(future.isDone())
+
+        System.out.println(future.get());
         service.shutdown();
 
- 
     }
 }
