7618d556ad58eb8bd349983e6f107f458698e944
Abhinav <ranaabhinav50@gmail.com>
2026-05-12 20:50:46 +0530

    Create ReverseSinglyLinkedList.java

---
+++ FastRevision/src/LinkedList/ReverseSinglyLinkedList.java
@@ -0,0 +1,27 @@
+package LinkedList;
+
+
+
+public class ReverseSinglyLinkedList {
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr!=null){
+            ListNode next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+
+
+        }
+        return prev;
+
+    }
+
+
+
+}
