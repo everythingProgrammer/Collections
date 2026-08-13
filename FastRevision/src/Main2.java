5dcdac7b65b1d12525e15f752a740c338391de03
Abhinav <ranaabhinav50@gmail.com>
2026-08-13 10:10:29 +0530

    unsure

--- FastRevision/src/Main2.java
+++ FastRevision/src/Main2.java
@@ -1,10 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main2 {
 
     final   double pi ;
 
-    public Main(){
+    public Main2(){
         pi = 3.14;
     }
     public static void main(String[] args) {
--- src/threading/interviewPractice/SyncronizedCounterIncrement.java
+++ src/threading/interviewPractice/SyncronizedCounterIncrement.java
@@ -13,6 +13,7 @@ public class SyncronizedCounterIncrement implements Runnable{
         for(int i  = 0; i<10000; i++) {
             synchronized (lock) {
                 counter++;
+                System.out.println(" Thread "+Thread.currentThread().getName());
             }
         }
     }
